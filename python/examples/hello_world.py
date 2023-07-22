bc4b0b357e2c3f30a952c73c35e19a9ac3ed4e35
Rudolf Olah <rudolf.olah.to@gmail.com>
2023-07-22 14:02:48 -0400

    examples

---
+++ python/examples/hello_world.py
@@ -0,0 +1,15 @@
+from chaincrafter import Chain, Prompt
+from chaincrafter.models import OpenAiChat
+
+chat_model = OpenAiChat(temperature=0.65, model_name="gpt-3.5-turbo")
+system_prompt = Prompt("You are a helpful assistant who responds to questions about the world")
+hello_prompt = Prompt("Hello, what is the capital of France? Answer only with the city name.")
+followup_prompt = Prompt("{city} sounds like a nice place to visit. What is the population of {city}?")
+chain = Chain(
+    system_prompt,
+    (hello_prompt, "city"),
+    (followup_prompt, "followup_response"),
+)
+messages = chain.run(chat_model)
+for message in messages:
+    print(f"{message['role']}: {message['content']}")
