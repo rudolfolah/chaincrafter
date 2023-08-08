ceed9d2062fbbc4f25cce4d080ef3252bf04ceee
Rudolf Olah <rudolf.olah.to@gmail.com>
2023-08-08 09:17:30 -0400

    python chaincrafter v0.2.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chaincrafter",
-    version="0.1.0",
+    version="0.2.0",
     description="Seamless integration and composability for large language model apps.",
     long_description="Seamless integration and composability for large language model apps.",
     author="Rudolf Olah",
